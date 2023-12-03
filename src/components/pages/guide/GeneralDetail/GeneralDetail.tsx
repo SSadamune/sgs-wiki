e49f9cfb022b8a1dbe4ed9ade50bbffb4a271ac4
Sadamune <zwiebelshi@gmail.com>
2023-12-04 00:31:36 +0900

    fix: error handler

--- src/components/pages/guide/GeneralDetail/GeneralDetail.tsx
+++ src/components/pages/guide/GeneralDetail/GeneralDetail.tsx
@@ -1,6 +1,7 @@
-import { General } from "@/data/types/Generals";
-import { VersionId } from "@/data/types/Version";
+import { General, GeneralVersion } from "@/data/types/Generals";
 import { useEffect, useMemo, useState } from "react";
+import Error from "next/error";
+import { VersionId } from "@/data/types/Version";
 
 type Props = {
   id: string;
@@ -12,31 +13,37 @@ export function GeneralDetail({ id }: Props) {
     useState<VersionId | null>(null);
 
   useEffect(() => {
-    import("@/data/generals")
-      .then((module) => {
-        const loadedGeneral = module.generals[id];
+    const fetchGeneralData = async () => {
+      try {
+        const allGeneralsData = await import("@/data/generals");
+        const loadedGeneral = allGeneralsData.generals[id];
+
         if (loadedGeneral) {
           setGeneralData(loadedGeneral);
           setDisplayedVersionId(loadedGeneral.defaultVersion);
         } else {
           console.error("武将 ID 不存在");
         }
-      })
-      .catch((err) => {
+      } catch (err) {
         console.error("无法加载数据:", err);
-      });
+      }
+    };
+
+    fetchGeneralData();
   }, [id]);
 
   const version = useMemo(() => {
-    if (displayedVersionId !== null) {
-      return generalData?.versions.filter(
-        (version) => version.versionId === displayedVersionId
-      )[0];
-    }
+    return generalData?.versions.find(
+      (v) => v.versionId === displayedVersionId
+    );
   }, [displayedVersionId, generalData?.versions]);
 
-  if (generalData === null) {
-    <div>Loading</div>;
+  if (!generalData) {
+    return <Error statusCode={404} title="武将信息未找到" />;
+  }
+
+  if (!version) {
+    return <Error statusCode={404} title="技能版本信息未找到" />;
   }
 
   return (
