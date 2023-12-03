14cbb3c866eaea0552910fb9616f2879d313b631
Sadamune <zwiebelshi@gmail.com>
2023-12-04 00:00:59 +0900

    filter generals

--- src/components/pages/guide/GeneralList/GeneralList.tsx
+++ src/components/pages/guide/GeneralList/GeneralList.tsx
@@ -1,15 +1,28 @@
 import Link from "next/link";
 import { generals as generalsData } from "@/data/generals";
+import { useMemo } from "react";
 
 export function GeneralList() {
+  const generalList = useMemo(
+    () =>
+      Object.entries(generalsData)
+        .filter((generalData) => generalData[1].defaultVersion !== "official")
+        .map(([key, generalData]) => ({
+          name: generalData.name,
+          faction: generalData.faction,
+          key,
+        })),
+    []
+  );
+
   return (
     <div>
       <h1>武将列表</h1>
       <ul>
-        {Object.entries(generalsData).map(([key, generalData]) => (
-          <li key={key}>
-            <Link href={`/guide/general/${generalData.id}`}>
-              {generalData.name} - {generalData.faction}
+        {generalList.map((general) => (
+          <li key={general.key}>
+            <Link href={`/guide/general/${general.key}`}>
+              {general.name} - {joinStrings(general.faction)}
             </Link>
           </li>
         ))}
@@ -17,3 +30,6 @@ export function GeneralList() {
     </div>
   );
 }
+
+const joinStrings = (toJoin: string | string[]) =>
+  Array.isArray(toJoin) ? toJoin.join("&") : toJoin;
