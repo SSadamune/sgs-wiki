7feda801f72efe18425eac6c2604b3f3c941a3bb
Sadamune <zwiebelshi@gmail.com>
2023-12-04 02:35:11 +0900

    a link with absolute path

--- src/pages/guide/general/index.tsx
+++ src/pages/guide/general/index.tsx
@@ -2,8 +2,7 @@ import { GeneralList } from "@/components/pages/guide/GeneralList";
 
 // TODO: common homepage
 const detailLink = (key: string): string =>
-  // `https://ssadamune.github.io/sgs-wiki/guide/general/${key}`;
-  `/guide/general/${key}`;
+  `https://ssadamune.github.io/sgs-wiki/guide/general/${key}`;
 
 export default function GeneralListPage() {
   return (
